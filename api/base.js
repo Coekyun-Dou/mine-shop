c9066bfe62a390f179dd6d927e687cbfe43655ad
Felix Du <less_duuuzx@163.com>
2025-09-20 23:03:49 +0800

    轮播图网络请求

--- api/base.js
+++ api/base.js
@@ -2,6 +2,6 @@
  * 存储接口地址
  */
 module.exports = {
-  baseUrl:"http://yunshangyouli.com:3001",   //公共地址
-  banner: "/api/banner",               //轮播图
+  baseUrl:"https://gitee.com/d___b",   //公共地址
+  banner: "/YSYL/raw/master/banner",               //轮播图
 }
