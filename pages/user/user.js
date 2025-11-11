3b6ea01ee78744053b494a2e3c9ca0b48d9b1534
less_duuuzx@163.com <Less_duuuzx@163.com>
2025-11-12 01:52:35 +0800

    优化user页面

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,38 +1,51 @@
 const { getLogin } = require("../../api/index.js")
+const { getCartCount } = require("../../utils/cart.js")
 
 Page({
     data: {
         userInfo: {},
-        list:[
+        orderCount: {
+            unpaid: 0,
+            unshipped: 0,
+            unreceived: 0,
+            unevaluated: 0
+        },
+        menuList: [
             {
-                text:"前端",
-                icon:"like",
-                color:"#ff0000"
+                text: "我的收藏",
+                icon: "like-o",
+                color: "#ff0000",
+                url: ""
             },
             {
-                text:"Python",
-                icon:"star",
-                color:"#2a83fe"
+                text: "优惠券",
+                icon: "coupon-o",
+                color: "#ff6b35",
+                url: ""
             },
             {
-                text:"Java",
-                icon:"fire",
-                color:"#fd6012"
+                text: "积分商城",
+                icon: "gem-o",
+                color: "#fd4d72",
+                url: ""
             },
             {
-                text:"大数据",
-                icon:"gem",
-                color:"#fd4d72"
+                text: "收货地址",
+                icon: "location-o",
+                color: "#2a83fe",
+                url: ""
             },
             {
-                text:"人工智能",
-                icon:"gift",
-                color:"#00b478"
+                text: "客服中心",
+                icon: "chat-o",
+                color: "#00b478",
+                url: ""
             },
             {
-                text:"算法u",
-                icon:"shop",
-                color:"#fe391f"
+                text: "关于我们",
+                icon: "info-o",
+                color: "#999999",
+                url: ""
             }
         ]
     },
@@ -41,6 +54,43 @@ Page({
         if(wx.getStorageSync('userInfo')){
             this.setData({
                 userInfo:wx.getStorageSync('userInfo')
+            })
+        }
+    },
+    onShow() {
+        // 每次显示页面时更新购物车数量
+        const cartCount = getCartCount()
+        this.setData({
+            cartCount: cartCount
+        })
+    },
+    // 点击订单状态
+    clickOrder(e) {
+        const type = e.currentTarget.dataset.type
+        wx.showToast({
+            title: "订单功能开发中",
+            icon: "none"
+        })
+    },
+    // 点击全部订单
+    clickAllOrders() {
+        wx.showToast({
+            title: "订单功能开发中",
+            icon: "none"
+        })
+    },
+    // 点击菜单项
+    clickMenu(e) {
+        const index = e.currentTarget.dataset.index
+        const item = this.data.menuList[index]
+        if (item.url) {
+            wx.navigateTo({
+                url: item.url
+            })
+        } else {
+            wx.showToast({
+                title: item.text + "功能开发中",
+                icon: "none"
             })
         }
     },
